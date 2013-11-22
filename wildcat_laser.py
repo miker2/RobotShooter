b068278ec77e867391738ef6ebc354418ac07af9
Michael Rose <michaelrose.vt@gmail.com>
2013-11-21 19:43:30 -0500

    Changed a number of things.  Laser and Robot collision detection done by pygame.sprite class. Baddies can now be shot at and destroyed.  Still some bugs in the laser motion.  Needs some fixing.. Also, the Badguy motion is pretty poor. Needs improvement.

--- wildcat_laser.py
+++ wildcat_laser.py
@@ -1,19 +1,29 @@
 import pygame
 import math
 
-class Laser:
+class Laser(pygame.sprite.Sprite):
     """
     Lasers for the wildcat robot
     """
 
-    def __init__(self,pos,yaw,vel,screen):
-        self.screen = screen
-        self.pos = list(pos)
-        self.yaw = yaw
-        self.vel = self.__rot2d((max(vel,20),0))
+    def __init__(self,actor,vel,screen,clock):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        # Create an empty surface for this Laser sprite
+        self.image = pygame.Surface((1,1))
+        self.rect = pygame.Rect(actor.pospx,(3,3))
+        self.rect.center = actor.pospx
+
+        self._screen = screen
+        self._clock  = clock
+        self._yaw = actor.yaw
+        self._vel = self.__rot2d((max(vel,20),0))
         self._age = 0
 
         self._oob = False
+
+        actor.reload()
+
+        #print "Laser created at ", self.rect.center
 
     @property
     def oob(self):
@@ -23,30 +33,40 @@ class Laser:
     def age(self):
         return self._age
 
-    def update(self,dt):
+    @property
+    def pos(self):
+        return self.rect.center
+
+    def update(self):
+        dt = self._clock.get_time() / 1000.0
         self._age += dt
 
-        dx = self.vel[0] * dt;
-        dy = self.vel[1] * dt;
+        dx = self._vel[0] * dt;
+        dy = self._vel[1] * dt;
 
-        self.pos[0] += dx
-        self.pos[1] += dy
+        self.rect.move_ip(dx,dy)
 
-        (px,py) = self.__rot2d([12,0])
-        px += self.pos[0]
-        py += self.pos[1]
-
-        pygame.draw.lines(self.screen, (255,0,0),False,[self.pos,[px,py]],2)
+        self.draw()
 
         self.__check_oob()
+        if self.oob:
+            self.kill()
+
+    def draw(self):
+        (px,py) = self.__rot2d([12,0])
+        px += self.rect.centerx
+        py += self.rect.centery
+
+        pygame.draw.lines(self._screen, (255,0,0),False,[self.rect.center,[px,py]],2)
 
     def __rot2d(self,p):
-        x_out = math.cos(self.yaw) * p[0] - math.sin(self.yaw) * p[1]
-        y_out = math.sin(self.yaw) * p[0] + math.cos(self.yaw) * p[1]
+        x_out = math.cos(self._yaw) * p[0] - math.sin(self._yaw) * p[1]
+        y_out = math.sin(self._yaw) * p[0] + math.cos(self._yaw) * p[1]
         return (x_out,y_out)
 
     def __check_oob(self):
-        self._oob &= self.pos[0] < 0
-        self._oob &= self.pos[0] > self.screen.get_width()
-        self._oob &= self.pos[1] < 0
-        self._oob &= self.pos[1] > self.screen.get_width()
+        self._oob = not self._screen.get_rect().collidepoint(self.rect.center)
+        #self._oob |= self._pos[0] < 0
+        #self._oob |= self._pos[0] > self.screen.get_width()
+        #self._oob |= self._pos[1] < 0
+        #self._oob |= self._pos[1] > self.screen.get_width()
