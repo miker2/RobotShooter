bd6cafa030611f2976ec57dd2e8be0e9cc0b1d35
Michael Rose <michaelrose.vt@gmail.com>
2013-12-17 12:43:17 -0500

    Mostly cleanup according to PyCharm's desires.

--- wildcat_laser.py
+++ wildcat_laser.py
@@ -1,17 +1,28 @@
-import pygame
 import math
-#from wildcat_driving_tester import Meter2PixSprite
+
+import pygame
+
+from wildcat_driving_tester import Meter2PixSprite
+
 
 class Laser(Meter2PixSprite):
     """
     Lasers for the wildcat robot
     """
 
-    def __init__(self,actor,vel,screen,clock):
+    def __init__(self, actor, vel, screen, clock):
+        """
+
+        @type self: Laser
+        @type actor: Meter2PixSprite
+        @type vel:
+        @type screen: pygame.Surface
+        @type clock: pygame.Clock
+        """
         Meter2PixSprite.__init__(self)
         # Create an empty surface for this Laser sprite
-        self.image = pygame.Surface((1,1))
-        self.rect = pygame.Rect(actor.pospx,(3,3))
+        self.image = pygame.Surface((1, 1))
+        self.rect = pygame.Rect(actor.pospx, (3, 3))
         self.rect.center = actor.pospx
 
         self._pos = actor.pos
@@ -20,7 +31,7 @@ class Laser(Meter2PixSprite):
         self._screen = screen
         self._clock  = clock
         self._yaw = actor.yaw
-        self._vel = self.__rot2d((max(vel,20),0))
+        self._vel = self.__rot2d((max(vel, 20), 0))
         self._age = 0
 
         self._oob = False
@@ -41,8 +52,8 @@ class Laser(Meter2PixSprite):
         dt = self._clock.get_time() / 1000.0
         self._age += dt
 
-        dx = self._vel[0] * dt;
-        dy = self._vel[1] * dt;
+        dx = self._vel[0] * dt
+        dy = self._vel[1] * dt
 
         self._pos[0] += dx
         self._pos[1] += dy
@@ -57,20 +68,16 @@ class Laser(Meter2PixSprite):
             self.kill()
 
     def draw(self):
-        (px,py) = self.__rot2d([12,0])
+        (px, py) = self.__rot2d([12, 0])
         px += self.rect.centerx
         py += self.rect.centery
 
-        pygame.draw.lines(self._screen, (255,0,0),False,[self.rect.center,[px,py]],2)
+        pygame.draw.lines(self._screen, (255, 0, 0), False, [self.rect.center, [px, py]], 2)
 
-    def __rot2d(self,p):
+    def __rot2d(self, p):
         x_out = math.cos(self._yaw) * p[0] - math.sin(self._yaw) * p[1]
         y_out = math.sin(self._yaw) * p[0] + math.cos(self._yaw) * p[1]
-        return (x_out,y_out)
+        return x_out, y_out
 
     def __check_oob(self):
         self._oob = not self._screen.get_rect().collidepoint(self.rect.center)
-        #self._oob |= self._pos[0] < 0
-        #self._oob |= self._pos[0] > self.screen.get_width()
-        #self._oob |= self._pos[1] < 0
-        #self._oob |= self._pos[1] > self.screen.get_width()
